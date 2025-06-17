0bb33d7f8ea4b901a7e2e63d2140f49532974c9a
Vineet Khisty <74192508+vineetkhisty@users.noreply.github.com>
2025-06-17 18:06:19 +0530

    Update aws-s3-backup.sh

--- Week2/aws-s3-backup.sh
+++ Week2/aws-s3-backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-src=$1
+src=$1 #Path of the source dir
 
-dest=$2
+dest=$2 # Path of the Destination Dir
 
-timestamp=$(date +%Y-%m-%d_%H:%M:%S)
+timestamp=$(date +%Y-%m-%d_%H:%M:%S) # Get the current date in YYY-MM-DD_H:M:S format
 
-zip "$dest/backup-$timestamp" $src
+zip "$dest/backup-$timestamp" $src # Creating a zip of the src dir into the dest dir
 
-aws s3 sync $dest s3://backup-vineet
+aws s3 sync $dest s3://backup-vineet # Push the zipped file on the S3 bucket
 
 echo "backup complete"
